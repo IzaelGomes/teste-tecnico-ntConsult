a0ca216521f3c6ced33ec91f1b6152e45e19c01d
Izael gomes <izaelgomes127@gmail.com>
2025-01-04 13:14:19 -0300

    chore: add new apis enpoints for dragons

--- src/services/dragon.ts
+++ src/services/dragon.ts
@@ -1,8 +1,26 @@
 import { api } from "../lib/axios";
 import { TDragon } from "./dragon-types";
 
-export const getAllDragons = () => {
-  const data = api.get<TDragon>("/dragon");
+export const getAllDragons = async () => {
+  const data = await api.get<TDragon[]>("/dragon");
 
   return data;
+};
+
+export const getDragon = async (id: string) => {
+  const data = await api.get<TDragon>(`/dragon/${id}`);
+
+  return data;
+};
+
+export const createDragon = async (id: string) => {
+  await api.post(`/dragon/${id}`);
+};
+
+export const updateDragon = async (id: string) => {
+  await api.put(`/dragon/${id}`);
+};
+
+export const deleteDragon = async (id: string) => {
+  await api.delete(`/dragon/${id}`);
 };
