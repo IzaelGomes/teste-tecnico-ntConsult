a1c3a2be205ce7849bce0e037008c007d2938396
Izael gomes <izaelgomes127@gmail.com>
2025-01-05 19:18:53 -0300

    refactor: remove implementation of delete card from component

--- src/pages/dragonsList/components/dragonCard/dragonCard.test.tsx
+++ src/pages/dragonsList/components/dragonCard/dragonCard.test.tsx
@@ -1,13 +1,25 @@
-import { describe, expect, test } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { describe, expect, test, vi } from "vitest";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import { DragonCard } from "./dragonCard";
 import { BrowserRouter } from "react-router-dom";
+import { PropsWithChildren } from "react";
+
+const AllProviders = ({ children }: PropsWithChildren) => {
+  return <BrowserRouter>{children}</BrowserRouter>;
+};
+
+const mockDragon = {
+  id: "5",
+  name: "Safari",
+};
 
 describe("DragonCard", () => {
-  test("it renders a dragon information", () => {
-    render(<DragonCard id="5" name="Safari" histories={[]} type="5" />, {
-      wrapper: BrowserRouter,
-    });
+  test("it should render the dragon card", () => {
+    render(
+      <AllProviders>
+        <DragonCard data={mockDragon} />
+      </AllProviders>
+    );
 
     expect(
       screen.getByRole("heading", { name: /safari/i })
@@ -15,5 +27,25 @@ describe("DragonCard", () => {
     const link = screen.getByRole("link", { name: /ver mais/i });
     expect(link).toHaveAttribute("href", "/dragons/details/5");
     expect(link).toHaveAccessibleDescription(/safari/i);
+  });
+
+  test("it should delete a dragon", async () => {
+    const mockOnDeleteDragon = vi.fn();
+
+    render(
+      <AllProviders>
+        <DragonCard data={mockDragon} onDelete={mockOnDeleteDragon} />
+      </AllProviders>
+    );
+
+    const deleteButton = screen.getByRole("button", {
+      name: /excluir/i,
+    });
+
+    fireEvent.click(deleteButton);
+
+    await waitFor(() => {
+      expect(mockOnDeleteDragon).toHaveBeenCalledWith("5");
+    });
   });
 });
--- src/pages/dragonsList/components/dragonCard/dragonCard.tsx
+++ src/pages/dragonsList/components/dragonCard/dragonCard.tsx
@@ -1,29 +1,39 @@
 import { Link } from "react-router-dom";
 import "./dragonCardStyles.css";
 import { useId } from "react";
+import { Pencil, Trash2 } from "lucide-react";
+import { TDragon } from "../../../../services/dragon-types";
 
+type DragonData = Pick<TDragon, "id" | "name">;
 interface DragonCardProps {
-  id: string;
-  name: string;
-  type: string;
-  histories: string[];
+  data: DragonData;
+  onDelete?: (id: string) => void;
 }
 
-export const DragonCard = ({ id, name }: DragonCardProps) => {
+export const DragonCard = ({ data, onDelete }: DragonCardProps) => {
   const titleId = useId();
+
   return (
     <div className="dragon-card">
       <h2 id={titleId} className="dragon-card__title">
-        🐉 Dragão - {name}
+        🐉 Dragão - {data.name}
       </h2>
-
       <Link
         aria-describedby={titleId}
         className="dragon-card__link"
-        to={`/dragons/details/${id}`}
+        to={`/dragons/details/${data.id}`}
       >
         ver mais..
       </Link>
+      <div className="dragon-card__actions">
+        <button aria-label="excluir" onClick={() => onDelete?.(data.id)}>
+          <Trash2 className="dragon-card__delete" />
+        </button>
+
+        <button>
+          <Pencil aria-label="editar" className="dragon-card__update" />
+        </button>
+      </div>
     </div>
   );
 };
