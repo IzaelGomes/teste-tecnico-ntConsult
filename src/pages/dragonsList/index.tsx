525e6b404b998af990f21f818fc24ef14981657e
Izael gomes <izaelgomes127@gmail.com>
2025-01-05 20:11:32 -0300

    refactor: dragon card props

--- src/pages/dragonsList/index.tsx
+++ src/pages/dragonsList/index.tsx
@@ -3,15 +3,21 @@ import { useGetDragons } from "../../services/hooks/useGetDragons";
 import "./style.css";
 import { orderDragonsByName } from "./utils";
 import { DragonCard } from "./components/dragonCard/dragonCard";
+import { useDeleteDragon } from "../../services/hooks/useDeleteDragon";
 
 export const Dragons = () => {
   const { data } = useGetDragons();
   const orderedDragons = useMemo(() => orderDragonsByName(data), [data]);
+  const { mutateAsync: deleteDragonMutate } = useDeleteDragon();
 
   return (
     <div className="dragon-container">
       {orderedDragons.map((dragon) => (
-        <DragonCard key={dragon.id} {...dragon} />
+        <DragonCard
+          key={dragon.id}
+          data={dragon}
+          onDelete={deleteDragonMutate}
+        />
       ))}
     </div>
   );
