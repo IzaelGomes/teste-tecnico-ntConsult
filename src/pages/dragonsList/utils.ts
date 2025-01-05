ac897d9b84d2099e4d19d5791328fe8e3e6a997a
Izael gomes <izaelgomes127@gmail.com>
2025-01-05 19:15:23 -0300

    fix: add lowercase on name comparison

--- src/pages/dragonsList/utils.ts
+++ src/pages/dragonsList/utils.ts
@@ -3,9 +3,9 @@ import { TDragon } from "../../services/dragon-types";
 export const orderDragonsByName = (dragons?: TDragon[]) => {
   return (
     dragons?.sort((a, b) => {
-      if (a.name.toLocaleUpperCase() > b.name.toLocaleUpperCase()) return 1;
+      if (a.name.toLowerCase() > b.name.toLowerCase()) return 1;
 
-      if (a.name < b.name) return -1;
+      if (a.name.toLowerCase() < b.name.toLowerCase()) return -1;
 
       return 0;
     }) ?? []
