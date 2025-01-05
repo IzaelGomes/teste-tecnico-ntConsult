1e8274185d52710a170cedf714cdee6684d1872c
Izael gomes <izaelgomes127@gmail.com>
2025-01-05 18:11:31 -0300

    fix: compare name with uppercase letter

--- src/pages/dragonsList/utils.ts
+++ src/pages/dragonsList/utils.ts
@@ -3,7 +3,7 @@ import { TDragon } from "../../services/dragon-types";
 export const orderDragonsByName = (dragons?: TDragon[]) => {
   return (
     dragons?.sort((a, b) => {
-      if (a.name > b.name) return 1;
+      if (a.name.toLocaleUpperCase() > b.name.toLocaleUpperCase()) return 1;
 
       if (a.name < b.name) return -1;
 
